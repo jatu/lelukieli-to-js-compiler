cec52274a7e51ce203430ec5e1c127eae77d5b44
jatu <jari.tuikka@gmail.com>
2016-11-13 21:48:52 +0200

    Add constructor parameter and secondary constructor to ConcreteSymbol

--- src/main/scala/Niko/ConcreteSymbol.scala
+++ src/main/scala/Niko/ConcreteSymbol.scala
@@ -1,8 +1,14 @@
 package Niko
 
+import sun.misc.Regexp
+
 /**
   * Created by nikoe on 13.11.2016.
   */
-class ConcreteSymbol extends Symbol {
+class ConcreteSymbol(regexp: Regexp) extends Symbol {
+
+  def this(regexpPattern: String) {
+    this(new Regexp(regexpPattern))
+  }
 
 }
