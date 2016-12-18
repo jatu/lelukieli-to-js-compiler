bd427b914d2727cb91782ac893d7541739df72b3
jatu <jari.tuikka@gmail.com>
2016-12-18 15:22:14 +0200

    Add while to language builder

--- src/main/scala/LanguageBuilder.scala
+++ src/main/scala/LanguageBuilder.scala
@@ -5,11 +5,13 @@ object LanguageBuilder {
     val stringValue = new TerminalSymbol("([\"'])(?:(?=(\\\\?))\\2.)*?\\1")
     val symbol = new TerminalSymbol("\\p{L}+")
     val assignment = new ComposedSymbol(List(symbol, new TerminalSymbol("="), value))
+    val condition = new ComposedSymbol(List(stringValue, new TerminalSymbol(" "), symbol, new TerminalSymbol(" "), stringValue))
 
     val print = new ComposedSymbol(List(new TerminalSymbol("tulosta"), new TerminalSymbol(" "), stringValue))
+    val wwhile = new ComposedSymbol(List(new TerminalSymbol("toista"), new TerminalSymbol(" "), condition, new TerminalSymbol(":")))
 
     //val loc = new ComposedSymbol(List(Self(), Self()), List(assignment))
-    val loc = new ComposedSymbol(List(assignment), List(print))
+    val loc = new ComposedSymbol(List(assignment), List(print), List(wwhile))
 
     val block = new ComposedSymbol(List(loc))
 
