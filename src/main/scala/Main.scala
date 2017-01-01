108a44a8be397d27bfbb7d715a9621952a7c44bc
jatu <jari.tuikka@gmail.com>
2017-01-01 17:17:00 +0200

    Add write file support

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,5 @@
+import java.io.{File, PrintWriter}
+
 import transform.LeluToJsTransform
 
 object Main {
@@ -7,8 +9,8 @@ object Main {
       Usage:
         app --help
         app -h
-        app --file code.lk
-        app -f code.lk
+        app --file code.lk app.js
+        app -f code.lk app.js
     """
 
   val compiler = new Compiler()
@@ -25,17 +27,33 @@ object Main {
   def makeChoice(args: Array[String]) {
     args(0) match {
       case "-h" | "--help" => println(usage)
-      case "-f" | "--file"  => compile(readFile(args(1)))
+      case "-f" | "--file"  => {
+        if(args(2) != null) {
+          compile(readFile(args(1)), args(2))
+        } else {
+          compile(readFile(args(1)), "output.js")
+        }
+      }
       case _ => println(usage)
     }
   }
 
-  def compile(param: String) = {
+  def compile(param: String, file: String) = {
     val resultCode = compiler.compile(LeluToJsTransform, param)
+    writeFile(resultCode, file)
   }
 
   def readFile(path: String): String = {
     scala.io.Source.fromFile(path).mkString
+  }
+
+  def writeFile(resultCode: Option[String], file: String) = {
+    if(resultCode.nonEmpty) {
+      val pw = new PrintWriter(new File(file))
+      val split = resultCode.get.split("\n")
+      split.foreach(pw.write)
+      pw.close()
+    }
   }
 
 }
