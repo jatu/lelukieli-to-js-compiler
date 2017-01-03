f1ab224797c8c131c586051cc434f06bdbbd9223
jatu <jari.tuikka@gmail.com>
2017-01-03 02:18:31 +0200

    Update javascript subset and language transform

--- src/main/scala/language/JavascriptSubset.scala
+++ src/main/scala/language/JavascriptSubset.scala
@@ -4,23 +4,34 @@ import symbol.{ComposedSymbol, ControlSymbol, DataSymbol, SymbolPool}
 
 object JavascriptSubset extends Language {
 
-  val value = new DataSymbol("true|false|null|[0-9]+\\.[0-9]*|[0-9]+")
+  val booleanValue = new DataSymbol("true|false")
+  val numValue = new DataSymbol("[0-9]+\\\\.[0-9]*|[0-9]+")
+  val nullValue = new DataSymbol("null")
   val stringValue = new DataSymbol("([\"'])(?:(?=(\\\\?))\\2.)*?\\1")
-  val symbol = new DataSymbol("\\p{L}+")
-  val assignment = new ComposedSymbol(symbol, new ControlSymbol("="), value)
-  val condition = new ComposedSymbol(stringValue, symbol, stringValue)
+  val value = new SymbolPool(booleanValue, numValue, nullValue, stringValue)
 
+  val expression = new SymbolPool()
   val mloc = new SymbolPool()
 
-  val print = new ComposedSymbol(new ControlSymbol("console.log"), new ControlSymbol("("), stringValue, new ControlSymbol(")"), new ControlSymbol(";"))
-  val wwhile = new ComposedSymbol(new ControlSymbol("while"), new ControlSymbol("("), condition, new ControlSymbol(")"), new ControlSymbol("{"), mloc, new ControlSymbol("}"))
-  val iff = new ComposedSymbol(new ControlSymbol("if"), new ControlSymbol("("), condition, new ControlSymbol(")"), new ControlSymbol("{"), mloc, new ControlSymbol("}"))
+  val symbol = new DataSymbol("\\p{L}+")
+  val functionCall = new ComposedSymbol(expression, new ControlSymbol("("), expression, new ControlSymbol(")"))
+  val functionReturnStatement = new ComposedSymbol(new ControlSymbol("return"), new ControlSymbol(" "), expression, new ControlSymbol(";"))
+  val functionB = new ComposedSymbol(new ControlSymbol("function"), new ControlSymbol("("), symbol, new ControlSymbol(")"), new ControlSymbol("{"), mloc, functionReturnStatement, new ControlSymbol("}"))
+  expression.addGroups(value, functionB, functionCall, symbol)
+  val assignment = new ComposedSymbol(symbol, new ControlSymbol("="), expression, new ControlSymbol(";"))
 
-  val loc = new SymbolPool(assignment, print, wwhile, iff)
+  val print = new ComposedSymbol(new ControlSymbol("console.log"), new ControlSymbol("("), expression, new ControlSymbol(")"), new ControlSymbol(";"))
+  val wwhile = new ComposedSymbol(new ControlSymbol("while"), new ControlSymbol("("), expression, new ControlSymbol(")"), new ControlSymbol("{"), mloc, new ControlSymbol("}"))
+  val iff = new ComposedSymbol(new ControlSymbol("if"), new ControlSymbol("("), expression, new ControlSymbol(")"), new ControlSymbol("{"), mloc, new ControlSymbol("}"))
+
+  val nonCatchedExpression = new ComposedSymbol(expression, new ControlSymbol(";"))
+
+  val loc = new ComposedSymbol(new SymbolPool(assignment, print, wwhile, iff, nonCatchedExpression), new ControlSymbol(";"))
   val locAndMloc = new ComposedSymbol(loc, mloc)
   mloc.addGroups(locAndMloc, loc)
   val block = new ComposedSymbol(mloc)
 
   override def startSymbol = block
+
 
 }
--- src/main/scala/transform/LeluToJsTransform.scala
+++ src/main/scala/transform/LeluToJsTransform.scala
@@ -1,7 +1,7 @@
 package transform
 
 import language.{JavascriptSubset, LeluLanguage}
-import symbol.AstNode
+import symbol.{AstDataLeaf, AstNode}
 
 object LeluToJsTransform extends Transform {
 
@@ -13,15 +13,28 @@ object LeluToJsTransform extends Transform {
   compiler.addTransition(LeluLanguage.block, compiler.equalTransition(JavascriptSubset.block))
   compiler.addTransition(LeluLanguage.locAndMloc, compiler.equalTransition(JavascriptSubset.locAndMloc))
   compiler.addTransition(LeluLanguage.assignment, compiler.equalTransition(JavascriptSubset.assignment))
-  compiler.addTransition(LeluLanguage.expression, compiler.equalTransition(JavascriptSubset.condition))
+  compiler.addTransition(LeluLanguage.expression, compiler.equalTransition(JavascriptSubset.expression))
   compiler.addTransition(LeluLanguage.iff, compiler.equalTransition(JavascriptSubset.iff))
   compiler.addTransition(LeluLanguage.wwhile, compiler.equalTransition(JavascriptSubset.wwhile))
   compiler.addTransition(LeluLanguage.loc, compiler.equalTransition(JavascriptSubset.loc))
   compiler.addTransition(LeluLanguage.mloc, compiler.equalTransition(JavascriptSubset.mloc))
   compiler.addTransition(LeluLanguage.print, compiler.equalTransition(JavascriptSubset.print))
   compiler.addTransition(LeluLanguage.stringValue, compiler.equalTransition(JavascriptSubset.stringValue))
+  compiler.addTransition(LeluLanguage.numValue, compiler.equalTransition(JavascriptSubset.numValue))
   compiler.addTransition(LeluLanguage.value, compiler.equalTransition(JavascriptSubset.value))
   compiler.addTransition(LeluLanguage.symbol, compiler.equalTransition(JavascriptSubset.symbol))
+  compiler.addTransition(LeluLanguage.functionB, compiler.equalTransition(JavascriptSubset.functionB))
+  compiler.addTransition(LeluLanguage.functionCall, compiler.equalTransition(JavascriptSubset.functionCall))
+  compiler.addTransition(LeluLanguage.functionReturnStatement, compiler.equalTransition(JavascriptSubset.functionReturnStatement))
+  compiler.addTransition(LeluLanguage.nonCatchedExpression, compiler.equalTransition(JavascriptSubset.nonCatchedExpression))
+  compiler.addTransition(LeluLanguage.nullValue, (astNode: AstNode) => List(AstDataLeaf(JavascriptSubset.nullValue, "null")))
+  compiler.addTransition(LeluLanguage.booleanValue, (astNode: AstNode) => {
+    if(astNode.asInstanceOf[AstDataLeaf].content == "suattaa") {
+      List(AstDataLeaf(JavascriptSubset.booleanValue, "true"))
+    } else {
+      List(AstDataLeaf(JavascriptSubset.booleanValue, "false"))
+    }
+  })
 
   def transform(astNode: AstNode) : Seq[AstNode] = {
     compiler.transform(astNode)
